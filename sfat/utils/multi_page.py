0b7f1365b23d14d319f859758c1ada31709c1bd0
pondelion <programming.deve@gmail.com>
2021-08-11 17:14:09 +0900

    add

---
+++ sfat/utils/multi_page.py
@@ -0,0 +1,19 @@
+from typing import Callable
+
+import streamlit as st
+
+
+class MultiPage:
+
+    def __init__(self):
+        self._pages = {}
+
+    def add_page(self, name, func: Callable[[], None]):
+        self._pages[name] = func
+
+    def run(self):
+        page = st.sidebar.radio(
+            'PAGE',
+            list(self._pages.keys())
+        )
+        self._pages[page]()
